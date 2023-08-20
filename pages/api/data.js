8e56b7472c06d09a4a0d527ff3ec1227ed6ee27e
Aravind <aravindgotojapan@gmail.com>
2023-08-20 09:13:00 +0530

    final

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,16 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
-
   try {
-    const data = await prisma.$queryRawUnsafe(`
-      SELECT * FROM Inventory
-      LIMIT ? OFFSET ?
-    `, limit, skip);
+    const data = await prisma.$queryRawUnsafe('SELECT * FROM Inventory;');
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
