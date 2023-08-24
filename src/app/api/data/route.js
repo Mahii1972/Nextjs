766b0dbba3ceded45c9c167ee428d04f46b1cf90
Aravind <aravindgotojapan@gmail.com>
2023-08-24 21:28:31 +0530

    Remove all files

--- src/app/api/data/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import getPSConnection from '@/lib/planetscaledb';
-
-export async function GET(request) {
-  const connection = await getPSConnection();
-  const [rows] = await connection.query('SELECT * FROM Inventory');
-  return NextResponse.json(rows, { status: 200 });
-}
